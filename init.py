cefb49952f48d2d0407d67ba158ac49a40255ea8
dilrong <wet932@naver.com>
2021-03-22 17:24:51 +0900

    Update: jsonify

--- init.py
+++ init.py
@@ -1,27 +1,28 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 from flask_cors import CORS
 from gensim.summarization.summarizer import summarize
-from newspaper import Article
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 CORS(app)
 
 
 @app.route("/", methods=['GET'])
 def home():
-    url = 'https://techcrunch.com/2021/03/16/patsnap-300-million-fundraise/'
-    news = Article(url, language='ko')
-    news.download()
-    news.parse()
-    return summarize(news.text)
+    return jsonify(
+        status=200,
+        message="Hello, Text Rank!"
+    )
 
 
 @app.route('/', methods=['POST'])
 def api_message():
     text = request.data.strip().decode('utf-8')
-    return summarize(text, ratio=0.1)
+    return jsonify(
+        status=200,
+        message=summarize(text, ratio=0.1)
+    )
 
 
 if __name__ == '__main__':
